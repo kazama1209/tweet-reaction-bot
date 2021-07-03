49117eeaa7837ea50b64e62515805d90e5456464
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2021-07-04 07:45:24 +0900

    Fix entry condition

--- opt/main.py
+++ opt/main.py
@@ -18,7 +18,8 @@ target_coin_names = [
 # 買い要因になりそうな単語（表記ゆれを考慮して複数指定）
 positive_words = [
     '買い',
-    'buy'
+    'buy',
+    'moon'
 ]
 
 # 売り要因になりそうな単語（表記ゆれを考慮して複数指定）
@@ -64,7 +65,16 @@ while True:
 
                 # ツイート内に対象の通貨名（条件1）＋買い要因or売り要因の単語（条件2）が含まれるかを確認（ex. ビットコイン 買い）
                 # 条件1と条件2は必ずセットである必要があり、どちらか1つだけでは成立しない
+
+                # 買い要因と売り要因、両方の単語が含まれていた場合は判断に困るのでパス
                 if any([target_coin_name in tweet.text.lower() for target_coin_name in target_coin_names]) \
+                    and any([positive_word in tweet.text.lower() for positive_word in positive_words]) \
+                    and any([negative_word in tweet.text.lower() for negative_word in negative_words]):
+                    
+                    pass 
+                
+                # 買い要因の単語が含まれていた場合は買い注文を出す
+                elif any([target_coin_name in tweet.text.lower() for target_coin_name in target_coin_names]) \
                     and any([positive_word in tweet.text.lower() for positive_word in positive_words]):
                     
                     # 成り行き買い注文
@@ -77,6 +87,7 @@ while True:
                     # 決済予定時刻を設定（現在時刻から〇分後）※ minutesに指定する値は任意でOK
                     close_time = now + datetime.timedelta(minutes = 5)
                 
+                # 売り要因の単語が含まれていた場合は売り注文を出す
                 elif any([target_coin_name in tweet.text.lower() for target_coin_name in target_coin_names]) \
                     and any([negative_word in tweet.text.lower() for negative_word in negative_words]):
                     
@@ -104,7 +115,7 @@ while True:
                 # 自分のポジション状況を確認
                 position = bybit_api.get_position(symbol)
 
-                # 買いポジションを持っていた場合は売り
+                # 買いポジションを持っていた場合は売り注文を出す
                 if position['side'] == 'Buy':
                     order = bybit_api.create_order(symbol, 'market', 'sell', amount)
                     print(f'Close: {now}')
@@ -112,7 +123,7 @@ while True:
                     # 決済が済んだらシステム終了
                     sys.exit()
                 
-                # 売りポジションを持っていた場合は買い
+                # 売りポジションを持っていた場合は買い注文を出す
                 elif position['side'] == 'Sell':
                     order = bybit_api.create_order(symbol, 'market', 'buy', amount)
                     print(f'Close: {now}')
